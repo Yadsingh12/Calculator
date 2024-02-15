b31e2f65cdc43ee4e00eb197d018b25cf5440ed4
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-15 20:46:55 +0530

    Add functions for addition, subtraction, multiplication and division

---
+++ script.js
@@ -0,0 +1,12 @@
+const addition = function(a,b) {
+    return (a+b);
+}
+const subtraction = function(a,b) {
+    return addition(a,-b);
+}
+const multiply = function(a,b) {
+    return a*b;
+}
+const divide = function(a,b) {
+    return (a/b);
+}
