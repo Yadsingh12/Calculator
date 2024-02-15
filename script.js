6d4fa77e8ff66cd916694f916dc1238501e5034c
Yadwinder Singh <143868203+Yadsingh12@users.noreply.github.com.>
2024-02-15 21:21:46 +0530

    Add an operate function to perform an operation on the operands.
    This functions calls the respective functions on basis of the operator provided.

--- script.js
+++ script.js
@@ -10,4 +10,23 @@ const multiply = function(a,b) {
 const divide = function(a,b) {
     return (a/b);
 }
+const operate = function(operand1, operand2, operator) {
+    let result;
+    switch(operator){
+        case "+":
+            result=addition(operand1, operand2);
+            break;
+        case "-":
+            result=subtraction(operand1, operand2);
+            break;
+        case "*":
+            result=multiply(operand1, operand2);
+            break;
+        case "/":
+            result=divide(operand1, operand2);
+            break;
+    }
+    return result;
+}
 let operand1, operand2, operator;
+console.log (operate(3,4,"*"));
